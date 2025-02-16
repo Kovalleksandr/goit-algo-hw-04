addc74964815a36ec0dff654994902d82161293c
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-02-16 09:38:10 +0200

    h_w_3

--- home_work_3/hw03.py
+++ home_work_3/hw03.py
@@ -16,24 +16,23 @@ def print_directory_structure(directory: Path, indent: str = ""):
         entries = sorted(directory.iterdir(), key=lambda e: (not e.is_dir(), e.name.lower()))
         for entry in entries:
             if entry.is_dir():
-                print(indent + Fore.BLUE + f"📂 {entry.name}")  # Синій для директорій
+                print(indent + Fore.BLUE + f"{entry.name}")  # Синій для директорій
                 print_directory_structure(entry, indent + "    ")  # Рекурсія
             else:
-                print(indent + Fore.GREEN + f"📜 {entry.name}")  # Зелений для файлів
+                print(indent + Fore.GREEN + f"{entry.name}")  # Зелений для файлів
     except PermissionError:
-        print(Fore.RED + "❌ Доступ заборонено")
+        print(Fore.RED + "Доступ заборонено")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(Fore.RED + "❌ Використання: python hw03.py <шлях_до_директорії>")
+        print(Fore.RED + "Використання: python hw03.py <шлях_до_директорії>")
         sys.exit(1)
 
     directory_path = Path(sys.argv[1])
 
     if not directory_path.exists():
-        print(Fore.RED + f"❌ Помилка: директорія {directory_path} не існує!")
+        print(Fore.RED + f"Помилка: директорія {directory_path} не існує!")
         sys.exit(1)
 
-    print(Fore.YELLOW + f"📦 {directory_path.resolve()}")
+    print(Fore.YELLOW + f"{directory_path.resolve()}")
     print_directory_structure(directory_path)
-#n
